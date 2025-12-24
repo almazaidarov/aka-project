8185220bcd8590a90577626bb989024e009edea0
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2025-12-23 18:05:32 -0600

    added s3 bucket

--- aka-project/main.tf
+++ aka-project/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-terraform-test-bucket-123456"
 }
--- aka-project/s3.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "aka_bucket" {
-  bucket = "my-test-bucket-123456789"
-}
