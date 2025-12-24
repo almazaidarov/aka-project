975de157eca8f2154326b589acc20e95d53c6037
ALMAZ <155114478+almazaidarov@users.noreply.github.com>
2025-12-23 18:04:49 -0600

    added provider

--- aka-project/provider.tf
+++ aka-project/provider.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,3 +10,4 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
